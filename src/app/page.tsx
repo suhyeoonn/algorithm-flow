38388c0f047e4d193673ed85443346ee630bf65e
suhyeoonn <suhyeon1740@gmail.com>
2024-11-14 13:53:11 +0900

    🎨 useReducer로 상태 관리하도록 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,66 +5,121 @@ import DownArrow from "@/components/icons/DownArrow";
 import UpArrow from "@/components/icons/UpArrow";
 import Log from "@/components/Log";
 import { motion } from "framer-motion";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useReducer } from "react";
 
 const MOVE = 40 + 5; // width + gap
-const N = 10;
+const N = 10; // TODO: 15로 변경하면 로그가 이상하게 표시된다
 const numbers = Array.from({ length: N }, (_, i) => i + 1);
+
+interface State {
+  sum: number;
+  start: number;
+  end: number;
+  log: ReactNode[];
+}
+
+type Action = { type: 0 } | { type: 1 } | { type: 2 };
+
+const initialState: State = {
+  sum: 0,
+  start: 1,
+  end: 1,
+  log: [],
+};
+
+const formatRangeSum = (start: number, end: number) =>
+  numbers.slice(start, end).join(" + ");
+
+const getSumHistory = (start: number, end: number) =>
+  formatRangeSum(start, end) + " = " + getSum(start, end);
+
+const getSum = (start: number, end: number) =>
+  numbers.slice(start, end).reduce((sum, n) => sum + n);
+
+const getChip = (start: number, end: number) => {
+  if (getSum(start - 1, end) === N) {
+    return <RedChip>sum == N</RedChip>;
+  } else if (getSum(start - 1, end) > N) {
+    return <BlueChip>{"sum > N"}</BlueChip>;
+  } else {
+    return <GreenChip>{"sum < N"}</GreenChip>;
+  }
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 0:
+      return {
+        ...state,
+        sum: state.sum + state.end,
+        end: state.end + 1,
+        log: [
+          ...state.log,
+          <Log key={`${state.start}_${state.end}`}>
+            <>
+              <span>sum: {getSumHistory(state.start - 1, state.end)}</span>
+              {getChip(state.start, state.end)}
+            </>
+          </Log>,
+        ],
+      };
+    case 1:
+      return {
+        ...state,
+        sum: state.sum - state.start,
+        start: state.start + 1,
+        log: [
+          ...state.log,
+          <Log key={`${state.start}_${state.end}`}>
+            <>
+              <span>sum: {getSumHistory(state.start - 1, state.end)}</span>
+              {getChip(state.start, state.end)}
+            </>
+          </Log>,
+        ],
+      };
+    case 2:
+      return {
+        ...state,
+        sum: state.sum + state.end,
+        end: state.end + 1,
+        log: [
+          ...state.log,
+          <Log key={`${state.start}_${state.end}`}>
+            <>
+              <span>sum: {getSumHistory(state.start - 1, state.end)}</span>
+              {getChip(state.start, state.end)}
+            </>
+          </Log>,
+        ],
+      };
+    default:
+      return state;
+  }
+}
+
 export default function Home() {
-  const [sum, setSum] = useState(0);
-  const [count, setCount] = useState(1);
-  const [start, setStart] = useState(1);
-  const [end, setEnd] = useState(1);
-  const [log, setLog] = useState<ReactNode[]>([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { sum, start, end, log } = state;
 
-  const formatRangeSum = (start: number, end: number) =>
-    numbers.slice(start, end).join(" + ");
-
-  const getSumHistory = (start: number, end: number) =>
-    formatRangeSum(start, end) + " = " + getSum(start, end);
-
-  const getSum = (start: number, end: number) =>
-    numbers.slice(start, end).reduce((sum, n) => sum + n);
-
-  // TODO: useReducer로 바꿀까?
   useEffect(() => {
     const interval = setInterval(() => {
+      if (end > N) {
+        clearInterval(interval);
+        return;
+      }
+
       if (sum === N) {
-        setCount((prevCount) => prevCount + 1);
-        setSum((prevSum) => prevSum + end);
-        setEnd((prevEnd) => prevEnd + 1);
+        dispatch({ type: 0 });
       } else if (sum > N) {
-        setSum((prevSum) => prevSum - start);
-        setStart((prevStart) => prevStart + 1);
-      } else if (sum < N && end < N) {
-        setSum((prevSum) => prevSum + end);
-        setEnd((prevEnd) => prevEnd + 1);
+        dispatch({ type: 1 });
+      } else if (sum < N) {
+        dispatch({ type: 2 });
       }
-    }, 2000);
-
-    if (end > N) {
-      clearInterval(interval);
-      console.log("최종 count:", count);
-    }
-
-    setLog((prevLog) => {
-      const newLog = [
-        <span key={`${start}_${end}`}>
-          sum: {getSumHistory(start - 1, end)}
-        </span>,
-      ];
-      if (getSum(start - 1, end) === N) {
-        newLog.push(<RedChip>sum == N</RedChip>);
-      } else if (getSum(start - 1, end) > N) {
-        newLog.push(<BlueChip>{"sum > N"}</BlueChip>);
-      } else {
-        newLog.push(<GreenChip>{"sum < N"}</GreenChip>);
-      }
-      return [...prevLog, <Log key={`${start}_${end}`}>{...newLog}</Log>];
-    });
+    }, 1000);
 
     return () => clearInterval(interval); // Cleanup으로 interval을 정리
-  }, [end, sum, count, start]); // 필요한 상태를 의존성 배열에 추가
+  }, [end, sum, start]);
 
   return (
     <div className="">
@@ -94,7 +149,7 @@ const ArrayAnimation = ({ start, end }: { start: number; end: number }) => {
   return (
     <div className="relative">
       <motion.div
-        className="absolute top-[-50px] flex flex-col justify-center items-center left-0"
+        className="absolute top-[-50px] flex flex-col justify-center items-center left-[1px]"
         animate={{
           x: (start - 1) * MOVE,
           transition: { duration: 1 },
@@ -103,9 +158,9 @@ const ArrayAnimation = ({ start, end }: { start: number; end: number }) => {
         start
         <UpArrow />
       </motion.div>
-      <ArrayDisplay length={N} />
+      <ArrayDisplay length={N + 1} disabledNum={N + 1} />
       <motion.div
-        className="absolute bottom-[-50px] flex flex-col justify-center items-center left-0"
+        className="absolute bottom-[-50px] flex flex-col justify-center items-center left-[5px]"
         animate={{
           x: (end - 1) * MOVE,
           transition: { duration: 1 },
--- src/components/ArrayDisplay.tsx
+++ src/components/ArrayDisplay.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-const ArrayDisplay = ({ length }: { length: number }) => {
+const ArrayDisplay = ({
+  length,
+  disabledNum,
+}: {
+  length: number;
+  disabledNum: number;
+}) => {
   const array = Array.from({ length }, (_, i) => i + 1);
   return (
-    <div className="flex gap-1">
+    <div className="flex gap-[5px]">
       {array.map((n) => (
         <div
           key={n}
-          className="w-10 h-10 leading-10 text-center border border-gray-400 rounded-lg"
+          className={`w-10 h-10 leading-10 text-center border ${
+            n >= disabledNum
+              ? "border-gray-700 text-gray-700"
+              : "border-gray-400"
+          } rounded-lg`}
         >
           {n}
         </div>
